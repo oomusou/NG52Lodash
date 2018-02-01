c08481e8a68ec658302e43351323924bf4944ad9
Oomusou Xiao <oomusou@gmail.com>
2018-02-01 15:03:29 +0800

    Setup Lodash in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(): void {
+    const scores: number[] = [100, 99, 98];
+
+    _.remove(scores, 2);
+    scores.forEach((score) => console.log(score));
+  }
 }
